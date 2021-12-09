ee5f98a448e4a547eca136d590b2bfa39d698d4e
Eduardo Baptista <eduardo.baptista@sinch.com>
2021-12-09 13:25:51 -0300

    Add validate functions

--- lib/sentinels.ex
+++ lib/sentinels.ex
@@ -1,18 +1,58 @@
 defmodule Sentinels do
   @moduledoc """
-  Documentation for `Sentinels`.
+  Help the validation and sanitization of request params.
   """
+
+  alias Ecto.Changeset
+  alias Sentinels.Error
+  alias Sentinels.Utils
 
   @doc """
-  Hello world.
+  Validate the request params.
 
-  ## Examples
+  ## Examples:
 
-      iex> Sentinels.hello()
-      :world
+  Using with types definitions:
 
+        iex> definitions = %{name: :string, age: :integer}
+        iex> params = %{"name" => "name", "age" => 25}
+        iex> Sentinels.validate(definitions, params)
+        {:ok, %{name: "name", age: 25}}
+
+  Using with `Sentinel` struct:
+
+        defmodule Validator do
+          use Sentinels.Sentinel
+
+            embedded_schema do
+              field :name, :string
+              field :age, :integer
+            end
+        end
+
+        iex> Sentinels.validate(validator, params)
+        iex> {:ok, %{name: "name", age: 25}}
   """
-  def hello do
-    :world
+  @spec validate(atom() | map(), map()) :: {:error, Error.t()} | {:ok, map()}
+  def validate(sentinel, data) when is_atom(sentinel) do
+    data
+    |> sentinel.changeset()
+    |> handle_changeset()
+  end
+
+  def validate(definition, data) when is_map(definition) do
+    keys = Map.keys(definition)
+
+    {%{}, definition}
+    |> Changeset.cast(data, keys)
+    |> Changeset.validate_required(keys)
+    |> handle_changeset()
+  end
+
+  defp handle_changeset(changeset) do
+    case Changeset.apply_action(changeset, :validate) do
+      {:ok, result} -> {:ok, Utils.struct_to_map(result)}
+      {:error, changeset} -> {:error, Error.new(changeset)}
+    end
   end
 end
